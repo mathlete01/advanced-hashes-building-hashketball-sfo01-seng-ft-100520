dcc63103cd9ec04347256ce06cb434f7f55f0f94
Matthew Sallin <msallin@gmail.com>
2020-09-28 03:42:59 +0000

    Automatically backed up by Learn

--- try.rb
+++ try.rb
@@ -2,9 +2,20 @@ require 'pry'
 
 def game_hash
   {home:
-    {team_name: "Brooklyn Nets",colors: ["Black, White"],players:
+    {team_name: "Brooklyn Nets",
+      colors: ["Black, White"],
+      players:
       {"Alan Anderson" =>
-        { number: 0,shoe: 16,points: 22,rebounds: 12,assists: 12,steals: 3,blocks: 1,slam_dunks: 1},
+        { number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+
       "Reggie Evans" =>
         { number: 30,
           shoe: 14,
